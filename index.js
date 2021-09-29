6d699999367fa288c8f4b9f61b056a18b111f94d
Alin Manole <alin.manole@yahoo.com>
2021-09-29 17:26:00 +0200

    5.25

---
+++ index.js
@@ -0,0 +1,128 @@
+// 1 - Nombre pairs 
+
+var total = 0
+
+for ( var i = 11 ; i < 47 ; i++ ) {
+    if ( i % 2 === 0 ) { 
+        console.log(i);
+        total = total + i;
+    }
+}
+
+console.log(total);
+
+
+// 2 - Nombre impairs
+
+var total = 0
+
+for ( var i = 109 ; i < 588 ; i++ ) {
+    if ( i % 2 === 1 ) {
+        console.log(i);
+        total = total + i;
+    }
+}
+
+console.log(total);
+
+// 3 - Puissance et racine carree
+
+var numbers = [ 1, 2, 3, 4, 5]
+var square = []
+var roots = []
+
+for ( var i = 1 ; i <= numbers.length ; i++) {
+    square.push(i ** 2)
+}
+    console.log(square)
+
+
+
+for ( var i = 0 ; i < square[i] ; i++ ) {
+     roots.push(Math.sqrt(square[i]))
+}
+  console.log(roots)
+
+
+  console.log("  ")
+// 4 - Table de multiplication v1
+
+
+for ( var i = 0 ; i <= 10; i++) {
+    console.log(  "3 x " ,i, " = " , i * 3)
+}
+
+console.log("  ")
+console.log("  ")
+console.log("  ")
+
+
+// 5 - Table de mutiplication v2 
+
+
+    for ( var i = 0 ; i < 1 ; i++ ){
+
+        for ( var j = 0 ; j <=10 ; j++ ){
+        console.log( "0 x " , j , " = " , j * 0 )
+        }
+        console.log("  ")
+        for ( var j = 0 ; j <=10 ; j++ ){
+            console.log( "1 x " , j , " = " , j * 1 )
+        }
+        console.log("  ")
+
+        for ( var j = 0 ; j <=10 ; j++ ){
+            console.log( "2 x " , j , " = " , j * 2 )
+        }
+
+        console.log("  ")
+
+        for ( var j = 0 ; j <=10 ; j++ ){
+            console.log( "3 x " , j , " = " , j * 3 )
+        }
+
+        console.log("  ")
+
+        for ( var j = 0 ; j <=10 ; j++ ){
+            console.log( "4 x " , j , " = " , j * 4 )
+        }
+        console.log("  ")
+
+        for ( var j = 0 ; j <=10 ; j++ ){
+            console.log( "5 x " , j , " = " , j * 5 )
+        }
+        console.log("  ")
+
+        for ( var j = 0 ; j <=10 ; j++ ){
+            console.log( "6 x " , j , " = " , j * 6 )
+        }
+        console.log("  ")
+
+        for ( var j = 0 ; j <=10 ; j++ ){
+            console.log( "7 x " , j , " = " , j * 7 )
+        }
+        console.log("  ")
+
+        for ( var j = 0 ; j <=10 ; j++ ){
+            console.log( "8 x " , j , " = " , j * 8 )
+        }
+        console.log("  ")
+
+        for ( var j = 0 ; j <=10 ; j++ ){
+            console.log( "9 x " , j , " = " , j * 9 )
+        }
+        console.log("  ")
+        for ( var j = 0 ; j <=10 ; j++ ){
+            console.log( "10 x " , j , " = " , j * 10 )
+        }
+        console.log("  ")
+    }
+
+    // 6 - Factorielle v1
+
+        var number = 7
+    
+    for ( var i = 7 - 1 ; i >= 1; i--){
+            number = number * i
+    }
+        console.log(number)
